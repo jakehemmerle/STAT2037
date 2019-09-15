df7b0e5523225255ba107cae650e3768e9d6450e
Jake Hemmerle <jakehemmerle@gmail.com>
2019-09-16 01:57:58 +0200

    added lots of functions

--- WebassignHelper.py
+++ WebassignHelper.py
@@ -6,9 +6,11 @@ exec(open('WebassignHelper.py').read())
 '''
 
 import collections
+import math
 
 # Paste those red numbers from Webassign in a file, call this function with filename.
 # Returns array with those numbers.
+# data = array_from_multiline_shitfile('shitfile.txt')
 def array_from_multiline_shitfile(filename):
   shitfile = open(filename, 'r')
   shitstring = shitfile.read()
@@ -17,17 +19,62 @@ def array_from_multiline_shitfile(filename):
   number_array = []
   for number in string_array:
     number_array.append(float(number))
+  number_array.sort()
 
   return number_array
 
-# Standard counting
+# Standard counting ()
 def get_frequency(number_array):
   return collections.Counter(number_array)
 
-# Frequency of item / total number of items
+# Get total samples from a number_array ADT
+def get_total_samples(number_array):
+  total_samples = 0
+  for item in number_array:
+    total_samples += number_array[item]
+  return total_samples
+
+# Frequency of item / total number of items (in a number_array ADT)
 def get_relative_frequency(number_array):
-  a = {}
+  total_samples = get_total_samples(number_array)
+  print("Total samples:  %d" % total_samples)
+
+  rel_freq = {}
   array_length = len(number_array)
   for number in number_array:
-    a[number] = number_array[number] / array_length
-  return a
+    rel_freq[number] = number_array[number] / total_samples
+  return rel_freq
+
+# sum of values / total values
+def get_mean(array):
+  sum = 0
+  for value in array:
+    sum += value
+  return sum / len(array)
+
+# get middle number
+def get_median(array):
+  '''
+  get the middle number
+
+  >>> get_median([2, 4, 6, 8, 10])
+  6.0
+
+  >>> get_median([2, 5, 7, 8, 9, 12, 13, 23, 65])
+  9.0
+
+  >>> get_median([2, 4, 6, 8, 10, 12])
+  7.0
+  '''
+
+  halfway_point = math.floor(len(array) / 2)
+  if (len(array) % 2 == 0):
+    return float( (array[halfway_point - 1] + array[halfway_point]) / 2 )
+  else:
+    return float( array[halfway_point] )
+  
+# subtract largest from smallest number
+def get_range(array):
+  if (len(array) <=1):
+    print("Error: array needs to have length of at least 2")
+  return array[len(array)-1] - array[0]
