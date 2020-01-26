357708effad9414a4adfa1aec4f638e0dfaa2dcc
Jake Hemmerle <jakehemmerle@gmail.com>
2020-01-26 13:04:03 -0500

    refactored functions to helper

--- module1/11.py
+++ module1/11.py
@@ -3,14 +3,13 @@ import sys
 # allows import of project files (idk how else to do this)
 sys.path.insert(1, '..')
 from utils.webassign import array_from_shitstring
-from stats import round_to_nearest_interval, median
+from stats import mean
 
-actual_pressure = array_from_shitstring(" 116.6 115.8 114.9 115.3 115.6 ")
-actual_pressure.sort()
-print("Actual pressure: {}".format(actual_pressure))
+youngs_mod = array_from_shitstring(" 116.6 115.8 114.9 115.3 115.6 ")
+youngs_mod.sort()
+print("Young's Mod: {}".format(youngs_mod))
 
-rounded_pressure = round_to_nearest_interval(actual_pressure, 5)
-print("Rounded pressure: {0}".format(rounded_pressure))
+sample_mean = mean(youngs_mod)
+print("Young's Mod (mean): {}".format(sample_mean))
 
-median_rounded_pressure = median(rounded_pressure)
-print("Median (rounded pressure): {0}".format(median_rounded_pressure))
+
--- stats.py
+++ stats.py
@@ -54,39 +54,8 @@ def get_range(array):
     return max(array) - min(array)
 
 
-def count_decimals(number):
-    # Returns number of decimals of a number
-    string = str(number)
-    if '.' not in string:
-        return 0
-    else:
-        return string[::-1].find('.')
-
-
 def sample_variance(array):
     # BROKEN?
     # Returns sample variance of the array
     data_mean = mean(array)
     return sum([(value - data_mean) ** 2 for value in array]) / len(array) - 1
-
-
-def round_to_nearest_interval(array, interval):
-    """
-    Rounds all values in an array to the nearest interval value
-    >>> round_to_nearest_interval([14, 33, 21], 5)
-    [15, 35, 20]
-
-    >>> round_to_nearest_interval([21, 24, 34], 2)
-    [22, 24, 34]
-    """
-    floored_array = [value - value % interval for value in array]
-    mod_array = [value % interval for value in array]
-    final_array = []
-
-    for base, mod in zip(floored_array, mod_array):
-        if mod >= interval / 2:
-            final_array.append(base + interval)
-        else:
-            final_array.append(base)
-
-    return final_array
