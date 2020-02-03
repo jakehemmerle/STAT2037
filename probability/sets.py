a3a548ad95a26d1986e5691db67d5b43cd89610d
Jake Hemmerle <jakehemmerle@gmail.com>
2020-02-02 22:46:21 -0500

    added event stuff and sets

--- probability/sets.py
+++ probability/sets.py
@@ -3,17 +3,29 @@ class Set:
     Generic set object. events should be number of events in the set
 
     """
+
     def __init__(self, values, name='generic'):
         self.values = values
 
 
 class Event(Set):
-    def __init__(self, identifier):
+    def __init__(self, identifier, probability=None):
         super().__init__(identifier)
+        self.probability = probability
 
 
 class SimpleEvent(Event):
-    def __init__(self, name, identifier):
+    def __init__(self, identifier, probability=None):
         super().__init__(identifier)
-        self.name = name
+        self.probability = probability
 
+
+class CompoundEvent(Event):
+    def __init__(self, identifier, events):
+        super().__init__(identifier)
+        self.events = []
+        for event in events:
+            self.add(event)
+
+    def add(self, event: SimpleEvent):
+        self.events.append(event)
