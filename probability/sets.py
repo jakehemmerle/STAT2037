d929ca83a8ba4af4a0f8808390c7717999f09e44
Jake Hemmerle <jakehemmerle@gmail.com>
2020-02-02 22:40:23 -0500

    started some set and event related objects for probability

--- probability/sets.py
+++ probability/sets.py
@@ -1,4 +1,19 @@
 class Set:
-    def __init__(self, size, name='generic'):
-        self.size = size
+    """
+    Generic set object. events should be number of events in the set
+
+    """
+    def __init__(self, values, name='generic'):
+        self.values = values
+
+
+class Event(Set):
+    def __init__(self, identifier):
+        super().__init__(identifier)
+
+
+class SimpleEvent(Event):
+    def __init__(self, name, identifier):
+        super().__init__(identifier)
+        self.name = name
 
